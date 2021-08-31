253f770d8a5c3a6eb2233d1c6288c5a630705a49
Quang Tran <nhatquang304@gmail.com>
2021-08-31 14:37:34 -0400

    add Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
 // import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+// import Search from "./components/Search";
+import Dropdown from "./Dropdown";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a frontend javascript framework",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite JS library among Engineers",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components",
-  },
-];
+// items for Accordion component
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a frontend javascript framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS library among Engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components",
+//   },
+// ];
 
 const App = () => {
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown />
     </div>
   );
 };
